8ef98d4b5b23b39d54750b06bf66c1e6b6888c54
Dmitry Jakovlev <jakovlev@ikunst.local>
2018-11-30 02:18:34 +0300

    Update script.js

--- 01_DOM/practice-1/js/script.js
+++ 01_DOM/practice-1/js/script.js
@@ -1,22 +1,11 @@
-//1
 document.body.style.backgroundColor = prompt('Какой будет фон у страницы?');
 
-//2
-const Page = document.querySelector('.page'); 
-Page.style.color = prompt('Какой будет цвет текста на странице?');
+document.querySelector('.page').style.color = prompt('Какой будет цвет текста на странице?');
 
-//3
-const Name = document.querySelector('.name');
-Name.innerHTML = (prompt('Как зовут человека, который вас вдохновляет?'));
+document.querySelector('.name').innerHTML = (prompt('Как зовут человека, который вас вдохновляет?'));
 
-//4
-const Image = document.querySelector('img');
-Image.setAttribute('src', prompt('Введите адрес картинки'));
+document.querySelector('img').setAttribute('src', prompt('Введите адрес картинки'));
 
-//5
-const About = document.querySelector('.shortBio');
-About.innerHTML = (prompt('Введите текст страницы'));
+document.querySelector('.shortBio').innerHTML = (prompt('Введите текст страницы'));
 
-//6
-const Animation = document.querySelector('.shortBio');
-Animation.className += ' animated';
+document.querySelector('.shortBio').className += ' animated';
